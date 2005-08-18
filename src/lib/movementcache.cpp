77abaadc531ca2bac750dcbf263a87ce15255087
Andreas Röver <roever@users.sourceforge.net>
2005-08-18 14:36:59 +0000

    comment the movement cache class

--- src/lib/movementcache.cpp
+++ src/lib/movementcache.cpp
@@ -1,5 +1,25 @@
+/* Burr Solver
+ * Copyright (C) 2003-2005  Andreas Röver
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
 #include <movementcache.h>
 
+/* the hash function. I don't know how well it performes, but it seems to be okay */
 static unsigned int hashValue(unsigned int s1, unsigned int s2, int dx, int dy, int dz, unsigned char t1, unsigned char t2, unsigned int tableSize) {
   unsigned int val = dx * 0x10101010;
   val +=             dy * 0x14814814;
@@ -14,31 +34,44 @@ static unsigned int hashValue(unsigned int s1, unsigned int s2, int dx, int dy, 
 int min(int a, int b) { if (a < b) return a; else return b; }
 int max(int a, int b) { if (a > b) return a; else return b; }
 
+/* double the hash table size and copy the old elements into
+ * the new table
+ */
 void movementCache::rehash(void) {
   unsigned int oldSize = tableSize;
 
+  /* the new size, roughly twice the old size but odd */
   tableSize = 2* tableSize + 1;
 
+  /* allocate new table */
   entry ** newHash = new entry * [tableSize];
   memset(newHash, 0, tableSize * sizeof(entry*));
 
+  /* copy the elements */
   for (unsigned int i = 0; i < oldSize; i++)
     while (hash[i]) {
+
+      /* remove from old table */
       entry * e = hash[i];
       hash[i] = e->next;
 
+      /* enter into new one */
       unsigned int h = hashValue(e->s1, e->s2, e->dx, e->dy, e->dz, e->t1, e->t2, tableSize);
       e->next = newHash[h];
       newHash[h] = e;
     }
 
+  /* delete the old table and make the new table the current one */
   delete [] hash;
   hash = newHash;
 }
 
+/* calculate the required movement possibilities */
 movementCache::entry * movementCache::calcValues(unsigned char s1, unsigned char t1,
                                                  unsigned int s2, unsigned int t2,
                                                  int dx, int dy, int dz) {
+
+  /* first get the shapes, create them when they are not available */
   const pieceVoxel_c * sh1 = shapes[s1][t1];
 
   if (!sh1) {
@@ -53,6 +86,7 @@ movementCache::entry * movementCache::calcValues(unsigned char s1, unsigned char
     shapes[s2][t2] = sh2;
   }
 
+  /* create the new have entry and fill some of its fields */
   entry * e = new entry;
 
   e->dx = dx;
@@ -65,6 +99,7 @@ movementCache::entry * movementCache::calcValues(unsigned char s1, unsigned char
   e->s1 = s1;
   e->s2 = s2;
 
+  /* calculate some bounding boxes for the intersecting and union boxes of the 2 pieces */
   int x1i, x2i, y1i, y2i, z1i, z2i;
 
   x1i = max(sh1->boundX1(), sh2->boundX1() + dx);
@@ -83,10 +118,25 @@ movementCache::entry * movementCache::calcValues(unsigned char s1, unsigned char
   z1u = min(sh1->boundZ1(), sh2->boundZ1() + dz);
   z2u = max(sh1->boundZ2(), sh2->boundZ2() + dz);
 
+  /* these will contain the result assume free movement for the beginning */
   int mx, my, mz;
-
   mx = my = mz = 32000;
 
+  /* now we want to calculate the movement possibilities for the x-axis
+   * we need to check the intersecting area of the y and z axis and the union
+   * area of the x axis.
+   *
+   * scan in the positive x-direction and search for the smallest gap between
+   * a cube of piece 1 and a sube in piece 2
+   * so if we find a cube in piece 1 on our way we reset the start gap marker (last)
+   * when we find a cube in the 2nd piece we look how long ago the last piece
+   * one hit we had, if that value is smaller than the saved one, we save that
+   *
+   * to avoid the the need for a check for the case that we need to hit a
+   * cube 1 first bevore calculating a gap size I initialize the gap
+   * start marker so that the resulting gap would be so big that it is bigger
+   * than the initial value
+   */
   for (int y = y1i; y <= y2i; y++)
     for (int z = z1i; z <= z2i; z++) {
 
@@ -103,6 +153,7 @@ movementCache::entry * movementCache::calcValues(unsigned char s1, unsigned char
       }
     }
 
+  /* same for y direction */
   for (int x = x1i; x <= x2i; x++)
     for (int z = z1i; z <= z2i; z++) {
 
@@ -115,6 +166,7 @@ movementCache::entry * movementCache::calcValues(unsigned char s1, unsigned char
           my = y-last-1;
     }
 
+  /* finally the z direction */
   for (int x = x1i; x <= x2i; x++)
     for (int y = y1i; y <= y2i; y++) {
 
@@ -127,6 +179,7 @@ movementCache::entry * movementCache::calcValues(unsigned char s1, unsigned char
           mz = z-last-1;
     }
 
+  /* check the result and put it into the hash node */
   assert((mx >= 0) && (my >= 0) && (mz >= 0));
 
   e->mx = mx;
@@ -138,13 +191,17 @@ movementCache::entry * movementCache::calcValues(unsigned char s1, unsigned char
 
 movementCache::movementCache(const puzzle_c * puzzle, unsigned int problem) {
 
+  /* initial table */
   tableSize = 101;
   hash = new entry * [tableSize];
-
   memset(hash, 0, tableSize * sizeof(entry*));
 
   entries = 0;
 
+  /* initialize the shape array with the shapes from the
+   * puzzle problem. the shape with transformation 0 is just
+   * a pointer into the puzzle, so don't delete them later on
+   */
   num_shapes = puzzle->probShapeNumber(problem);
 
   shapes = new const pieceVoxel_c ** [num_shapes];
@@ -154,6 +211,7 @@ movementCache::movementCache(const puzzle_c * puzzle, unsigned int problem) {
     shapes[s][0] = puzzle->probGetShapeShape(problem, s);
   }
 
+  /* initialize the piece array */
   pieces = new unsigned int [puzzle->probPieceNumber(problem)];
 
   int pos = 0;
@@ -165,6 +223,7 @@ movementCache::movementCache(const puzzle_c * puzzle, unsigned int problem) {
 
 movementCache::~movementCache() {
 
+  /* delete the hash nodes */
   for (unsigned int i = 0; i < tableSize; i++)
     while (hash[i]) {
       entry * e = hash[i];
@@ -173,21 +232,27 @@ movementCache::~movementCache() {
       delete e;
     }
 
-  delete [] hash;
-  delete [] pieces;
-
+  /* the shape with transformation 0 is just
+   * a pointer into the puzzle, so don't delete them
+   *
+   * but all the others are created by us, so we free them
+   */
   for (unsigned int s = 0; s < num_shapes; s++) {
     for (unsigned int t = 1; t < NUM_TRANSFORMATIONS; t++)
       if (shapes[s][t])
         delete shapes[s][t];
     delete [] shapes[s];
   }
+
   delete [] shapes;
+  delete [] hash;
+  delete [] pieces;
 }
 
 void movementCache::getValue(int dx, int dy, int dz, unsigned char t1, unsigned char t2, unsigned int p1, unsigned int p2,
          int * mx, int * my, int * mz) {
 
+  /* find out the shapes that the pieces have */
   unsigned int s1 = pieces[p1];
   unsigned int s2 = pieces[p2];
 
@@ -195,25 +260,33 @@ void movementCache::getValue(int dx, int dy, int dz, unsigned char t1, unsigned 
 
   entry * e = hash[h];
 
+  /* check the list of nodes in the current hash bucket */
   while (e && ((e->dx != dx) || (e->dy != dy) || (e->dz != dz) ||
                (e->t1 != t1) || (e->t2 != t2) || (e->s1 != s1) || (e->s2 != s2)))
     e = e->next;
 
-
+  /* check, if we found the required node */
   if (!e) {
 
+    /* no is not found, enter a new node into the table */
+
+    /* first increase the number of entries, in also the table
+     * size, if required */
     entries++;
     if (entries > tableSize) {
       rehash();
+      /* after the resize we need to recalculate the hash for the
+       * new table size */
       h = hashValue(s1, s2, dx, dy, dz, t1, t2, tableSize);
     }
 
-    /* calculate values */
+    /* calculate values and enter them into the table */
     e = calcValues(s1, t1, s2, t2, dx, dy, dz);
     e->next = hash[h];
     hash[h] = e;
   }
 
+  /* return the values */
   *mx = e->mx;
   *my = e->my;
   *mz = e->mz;
--- src/lib/movementcache.h
+++ src/lib/movementcache.h
@@ -24,39 +24,79 @@
 #include "voxel.h"
 #include "puzzle.h"
 
+
+/* this class calculates the possible movement between 2 pieces
+ * because that calculation is relatively expensive it caches
+ * that value. Thats the reason for the name
+ */
 class movementCache {
 
+  /* values are saved within a hash table, this is the
+   * entry for the table
+   */
   typedef struct entry {
+
+    /* position of piece two relative to piece one */
     int dx, dy, dz;
+
+    /* the possible movement in positive directions */
     unsigned int mx, my, mz;
+
+    /* the 2 shapes */
     unsigned int s1, s2;
+
+    /* for the linked list in the hash table */
     struct entry * next;
+
+    /* the transformations of the 2 involved pieces
+     * normally we would need only one transformation, that for piece 2
+     * but the calculations involved to transform the 2 pieces so that
+     * piece one has a fixed transformation are too expensive
+     */
     unsigned short t1, t2;
   } entry;
 
-
+  /* the hash table */
   entry ** hash;
 
+  /* the size of the table and the number of entries within the table */
   unsigned int tableSize;
   unsigned int entries;
 
+  /* the shapes in all possible transformations. The voxel spaces are
+   * calculated on demand. The entry at the zero-th position are
+   * pointers into the puzzle, so we must not free them
+   */
   const pieceVoxel_c *** shapes;
+
+  /* the mapping of pieces to shape numbers */
   unsigned int * pieces;
 
+  /* number of shapes */
   unsigned int num_shapes;
 
-
+  /* this function resizes the hash table to roughly twice the size */
   void rehash(void);
 
+  /* when the entry is not inside the table, this function calculates the values */
   entry * calcValues(unsigned char s1, unsigned char t1, unsigned int s2, unsigned int t2,
                      int dx, int dy, int dz);
 
 public:
 
+  /* create the cache, the cache is then fixed to the puzzle and the problem, it can
+   * and should be reused to analyse all assemblies found but can not be used for another puzzle
+   */
   movementCache(const puzzle_c * puz, unsigned int problem);
 
   ~movementCache(void);
 
+  /* return the values, that are:
+   * how far can the 2nd piece be ove in positive x, y and z direction, when
+   * the 2nd piece is ofset by dx, dy and dz relative to the first,
+   * the 2 pieces are the pieces p1 and p2 from the puzzle and problem defined in the constructor
+   * and the 2 pieces are transformed by t1 and t2
+   */
   void getValue(int dx, int dy, int dz, unsigned char t1, unsigned char t2, unsigned int p1, unsigned int p2,
                 int * mx, int * my, int * mz);
 };
