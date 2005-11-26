74794b8a5dc2c4e06b14606e60db6319c215d6ec
Andreas Röver <roever@users.sourceforge.net>
2005-11-26 11:25:56 +0000

    a few more options for export window

--- src/gui/ImageExport.cpp
+++ src/gui/ImageExport.cpp
@@ -32,35 +32,55 @@ ImageExportWindow::ImageExportWindow(void) {
   new LFl_Round_Button("3x3 Supersampling", 0, 2);
   new LFl_Round_Button("4x4 Supersampling", 0, 3);
   new LFl_Round_Button("5x5 Supersampling", 0, 4);
-  (new LFl_Box(0, 5))->weight(0, 1);
+  new LFl_Round_Button("Use piece colors", 0, 5);
+  new LFl_Round_Button("Use color constraint colors", 0, 6);
+  new LFl_Check_Button("Dim static pieces", 0, 7);
+  (new LFl_Box(0, 8))->weight(0, 1);
   fr->end();
 
   // user defined size input
   fr = new LFl_Frame(1, 1, 1, 2);
-  (new LFl_Box("Size X", 0, 0))->stretchRight();
-  (new LFl_Box("Size Y", 0, 1))->stretchRight();
-  (new LFl_Box("DPI", 0, 2))->stretchRight();
-  (new LFl_Box("Pixel X", 0, 3))->stretchRight();
-  (new LFl_Box("Pixel Y", 0, 4))->stretchRight();
+  new LFl_Round_Button("A4 Portrait", 0, 0, 5, 1);
+  new LFl_Round_Button("A4 Landscape", 0, 1, 5, 1);
+  new LFl_Round_Button("manual", 0, 2, 5, 1);
+  (new LFl_Box("Size X", 0, 3))->stretchRight();
+  (new LFl_Box("Size Y", 0, 4))->stretchRight();
+  (new LFl_Box("DPI", 0, 5))->stretchRight();
+  (new LFl_Box("Pixel X", 0, 6))->stretchRight();
+  (new LFl_Box("Pixel Y", 0, 7))->stretchRight();
 
-  (new LFl_Box(1, 0))->setMinimumSize(2, 0);
+  (new LFl_Box(1, 3))->setMinimumSize(2, 0);
 
-  new LFl_Input(2, 0);
-  new LFl_Input(2, 1);
-  new LFl_Input(2, 2);
   new LFl_Input(2, 3);
   new LFl_Input(2, 4);
+  new LFl_Input(2, 5);
+  new LFl_Input(2, 6);
+  new LFl_Input(2, 7);
 
-  (new LFl_Box(3, 0))->setMinimumSize(2, 0);
+  (new LFl_Box(3, 3))->setMinimumSize(2, 0);
 
-  new LFl_Box("mm", 4, 0);
-  new LFl_Box("mm", 4, 1);
+  new LFl_Box("mm", 4, 3);
+  new LFl_Box("mm", 4, 4);
 
-  (new LFl_Box(0, 5))->weight(0, 1);
+  (new LFl_Box(0, 8))->weight(0, 1);
 
   fr->end();
 
-  layouter_c * l = new layouter_c(0, 3, 2, 1);
+  fr = new LFl_Frame(0, 3, 2, 1);
+
+  new LFl_Box("File name", 0, 0);
+  new LFl_Box("Path", 0, 1);
+  new LFl_Box("Number of files", 0, 2, 2, 1);
+  new LFl_Box("Number of images", 0, 3, 2, 1);
+
+  new LFl_Input(1, 0, 2, 1);
+  new LFl_Input(1, 1, 2, 1);
+  new LFl_Input(2, 2);
+  new LFl_Input(2, 3);
+
+  fr->end();
+
+  layouter_c * l = new layouter_c(0, 4, 2, 1);
   (new LFl_Box())->weight(1, 0);
   (new LFl_Button("Export Image(s)", 1, 0))->pitch(7);
   (new LFl_Button("Abbort", 2, 0))->pitch(7);
