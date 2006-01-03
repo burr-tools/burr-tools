c96463ba59797fe05249bda135995b49b50ec8ef
Andreas Röver <roever@users.sourceforge.net>
2006-01-03 18:09:58 +0000

    remove the inner white frame from the box selector

--- src/gui/BlockList.cpp
+++ src/gui/BlockList.cpp
@@ -161,8 +161,6 @@ void SelectableTextList::blockDraw(unsigned int block, int x, int y) {
     fl_rect(x+1, y+1, w-2, h-2);
     fl_color(0, 0, 0);
     fl_rect(x+2, y+2, w-4, h-4);
-    fl_color(255, 255, 255);
-    fl_rect(x+3, y+3, w-6, h-6);
   }
 }
 
