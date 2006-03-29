bf4af6c968350248ab552c3e6b3569642b21be31
Andreas Röver <roever@users.sourceforge.net>
2006-03-29 15:19:39 +0000

    some comments and cleanup to the grideditor header file

--- src/gui/grideditor.h
+++ src/gui/grideditor.h
@@ -45,7 +45,11 @@ public:
     TSK_COLOR
   } enTask;
 
-  /* some editing tools */
+  void setTask(enTask t) { task = t; }
+
+  /* some editing tools these might change with the inherited classes
+   * but right now we keep it in here and have just one ste of editing tools
+   */
   enum {
     TOOL_MIRROR_X = 1,
     TOOL_MIRROR_Y = 2,
@@ -55,11 +59,17 @@ public:
     TOOL_STACK_Z = 32
   };
 
+  void activateTool(int tool) { activeTools |= tool; }
+  void deactivateTool(int tool) { activeTools &= ~tool; }
+  void setTool(int tool) { activeTools = tool; }
+
   /* edit types */
   enum {
     EDT_SINGLE,
     EDT_RUBBER
   };
+
+  void setEditType(int type) { editType = type; }
 
 protected:
 
@@ -90,8 +100,16 @@ protected:
   unsigned char activeTools;
   int editType;
 
+  bool setLayer(unsigned int zv);
   int handle(int event);
   void draw();
+
+protected:
+
+  /* tool function to calculate, if the given voxel coordinate x;y is inside
+   * the specified area
+   */
+  bool inRegion(int x, int y, int x1, int x2, int y1, int y2, int sx, int sy, int mode);
 
 public:
 
@@ -104,16 +122,12 @@ public:
   unsigned int getZ(void) { return currentZ; }
 
   // sets the color to use for editing voxels
-  void setColor(unsigned int col) {
-    currentColor = col;
-  }
+  void setColor(unsigned int col) { currentColor = col; }
 
   // sets the voxel space to edit, the widget doesn't take over the space
   // the voxelspace must not be deleted while this is set here
   void setPuzzle(puzzle_c * p, unsigned int piecenum);
   void clearPuzzle();
-
-  void setTask(enTask t) { task = t; }
 
   // get the mouse mosition so that the cursor can be shown in 3d view
   bool getMouse(void) { return inside; }
@@ -125,17 +139,11 @@ public:
 
   // find out the reason why this widget called the callback
   enum {
-    RS_MOUSEMOVE,
-    RS_CHANGESQUARE
+    RS_MOUSEMOVE,     // the mouse moved, the cursor must be updated
+    RS_CHANGESQUARE   // something was edited, 3D view must be redrawn
   };
 
   int getReason(void) { return callbackReason; }
-
-  void activateTool(int tool) { activeTools |= tool; }
-  void deactivateTool(int tool) { activeTools &= ~tool; }
-  void setTool(int tool) { activeTools = tool; }
-
-  void setEditType(int type) { editType = type; }
 
   /* each grid editor has to provide this function
    * is calculates the grid position gx and gy inside the voxel
@@ -143,20 +151,37 @@ public:
    */
   virtual void calcGridPosition(int x, int y, int *gx, int *gy) = 0;
 
+  /* calculate the x and sy scaling and the x and y translation for
+   * the current shape so that it fits into the available space and
+   * is well centered, the returned values will be given
+   * to all drawing functions so that they do know where to paint
+   */
+  virtual void calcParameters(int *sx, int *sy, int *tx, int *ty) = 0;
+
+  /* the following function all do draw a little bit of one tile
+   * The first draws the color part of a normal tile, this should fill
+   * out the tile completely, the 2nd draws the color part of a variable tile
+   * this should leave 3 pixels empty to the edge of the tile. Both functions
+   * do not draw the frame around the tile, just the inside. You don't need
+   * to take care of the color, just draw
+   *
+   * If follows the frame drawing function. Just draw a frame around the tile
+   * keep in mind to properly merge with the neibors otherwise the frames might
+   * be 2 pixels thick
+   *
+   * Then we have the tile color. Here you draw the assignd color constraint color
+   * marker. This should be placed within one corner of the tile. Just draw it and
+   * don't worry for the color
+   *
+   * Finally the cursor. Right now its completely up to you do completely draw
+   * it. Eventually I will provide a better way to do that
+   */
   virtual void drawNormalTile(int x, int y, int tx, int ty, int sx, int sy) = 0;
   virtual void drawVariableTile(int x, int y, int tx, int ty, int sx, int sy) = 0;
   virtual void drawTileFrame(int x, int y, int tx, int ty, int sx, int sy) = 0;
   virtual void drawTileColor(int x, int y, int tx, int ty, int sx, int sy) = 0;
   virtual void drawTileCursor(int x, int y, int x1, int y1, int x2, int y2, int tx, int ty, int sx, int sy) = 0;
 
-  virtual void calcParameters(int *sx, int *sy, int *tx, int *ty) = 0;
-
-  bool setLayer(unsigned int zv);
-
-  /* tool function to calculate, if the given voxel coordinate x;y is inside
-   * the specified area
-   */
-  bool inRegion(int x, int y, int x1, int x2, int y1, int y2, int sx, int sy, int mode);
 };
 
 #endif
