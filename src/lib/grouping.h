04abd0ea65fbdaac06ef24c8a0702ac070f6fbdd
Andreas Röver <roever@users.sourceforge.net>
2009-01-31 14:31:57 +0000

    doxygenize grouping.h

--- src/lib/grouping.h
+++ src/lib/grouping.h
@@ -18,14 +18,20 @@
 #ifndef __GROUPING_H__
 #define __GROUPING_H__
 
-/* problem: when trying to disassemble a puzzle with groups
- * there occurs a problem when identical pieces belong to different groups
- * because you don't know, to which group the current piece belongs to
+#include <vector>
+
+/**
+ * Solves the grouping problem
  *
- * but this groups assignment might change the disassembability of the
+ * Problem: when trying to disassemble a puzzle with groups there
+ * comes up the problem when identical pieces belong to different groups.
+ * Because you don't know, to which group the current piece belongs to
+ * you don't know what to do.
+ *
+ * But the groups assigned to a piece might change the disassembability of the
  * whole puzzle
  *
- * example a puzzle made out of 8 identical pieces, 4 belong to group A and
+ * Example a puzzle made out of 8 identical pieces, 4 belong to group A and
  * the other 4 to group B. The pieces are assembled into such a shape
  * that always 4 do interlock and can not be disassembled. Depending
  * on how the pieces from the 2 groups are distributed between the
@@ -34,17 +40,15 @@
  * The functionality in here is supposed to help solve this problem
  * when the disassembler goes along it's way and tries to disassemble
  * a (sub)problem it only stops premature when there is a clear group
- * assignment available, meaning all pieces have a unique group assigned
+ * assignment available, meaning all pieces have a unique group assigned.
  *
- * otherwise it tries to disassemble. If this is not possible it
+ * Otherwise it tries to disassemble. If this is not possible it
  * checks with the functions in here, if it is possible to assign
  * groups to the pieces in such a way that all the involved pieces
  * are in the same group. The class will allocate the groups to the
  * involved pieces and use this knowledge later on when more groups
- * get added
+ * get added.
  */
-
-#include <vector>
 
 class grouping_c {
 
@@ -83,26 +87,30 @@ public:
 
   grouping_c(void) : numGroups(0), failed(false) {}
 
-
-  /* initialisation:
+  /**
+   * Initialisation.
    * the following functions tell the class what pieces can be in what groups
    * and how many of them are available
    */
   void addPieces(unsigned int pc, unsigned int group, unsigned int count);
 
-  /* remove all sets and start fresh */
+  /** remove all sets and start fresh */
   void reSet(void);
 
-  /* using:
-   * start a new set of pieces
-   * add the pieces you need to this set, each time you add a new piece
+  /**
+   * start a new set of pieces.
+   */
+  void newSet(void);
+
+  /**
+   * Add the pieces you need to the current set. Each time you add a new piece
    * you get a return value saying if it can still be done, as soon
    * as you get false from that function you must stop, it can not
    * be true later any way and the function will return unexpected
    * results after the first false
    */
-  void newSet(void);
   bool addPieceToSet(unsigned int pc);
 };
 
 #endif
+
