f7af23e28eac66bca53c4c1ce84616da53a98423
Andreas Röver <roever@users.sourceforge.net>
2009-03-08 20:31:54 +0000

    cleanup of makefile to make sure the right files get added to distribution
    - move the source files dor th edocumentation in their own directory
    - the doc subdir now contains only the conversion script and the generated stuff that will not go int dist file
    - also correct generator scrips and dotygen.cfg, those should NOT go into the documentation

--- doc/convert.sh
+++ doc/convert.sh
@@ -1,9 +1,15 @@
+# copy images from other directory to here
+for i in ../doc_src/*.png
+do
+  cp $i .
+done
+
 # create a table of content for easier navigation in the t2t file
-./txt2tags.py --toc-only userGuide.t2t > userGuide.t2t.toc
+../doc_src/txt2tags.py --toc-only ../doc_src/userGuide.t2t > userGuide.t2t.toc
 
 # create a pfd by first converting t2t to tex file and then running pdflatex
 # run pdflatex 4 times to be sure all references are correct
-./txt2tags.py -t tex --toc -n userGuide.t2t
+../doc_src/txt2tags.py -t tex --toc -n -o - ../doc_src/userGuide.t2t > userGuide.tex
 pdflatex userGuide.tex
 pdflatex userGuide.tex
 pdflatex userGuide.tex
@@ -22,5 +28,5 @@ else
   echo creating html dir
   mkdir html
 fi
-./txt2tags.py -t html userGuide.t2t
+../doc_src/txt2tags.py -t html -o - ../doc_src/userGuide.t2t > userGuide.html
 htmldoc --format htmlsep --toclevels 2 --outdir html userGuide.html
