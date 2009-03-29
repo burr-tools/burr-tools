1ee493fabdc6192d70c60e8180a712671703b641
Andreas Röver <roever@users.sourceforge.net>
2009-03-29 09:55:27 +0000

    doxygen the xml stuff a bit

--- src/lib/xml.h
+++ src/lib/xml.h
@@ -24,13 +24,14 @@
 
 #include <map>
 
-/** /file
+/** \file
  * This file contains the XML load and save stuff.
  *
  * The load functions are base on xmlpat and the save stuff is self written
  * I used to use xml2 with xmlwrapp, but that got way to bulky and slow
  */
 
+/** this exception is thrown by xmlWriter_c, when it is used in a wrong way */
 class xmlWriterException_c {
 
   public:
@@ -39,6 +40,7 @@ class xmlWriterException_c {
 
 };
 
+/** a simple class for xml file generation */
 class xmlWriter_c
 {
   private:
@@ -47,12 +49,14 @@ class xmlWriter_c
      */
     std::ostream & stream;
 
+    /** the stack of currently open tags */
     std::vector<std::string> tagStack;
 
+    /** the internal state of the writer */
     enum {
-      StBase,
-      StInTag,
-      StInContent,
+      StBase,        ///< right after start
+      StInTag,       ///< inside an open tag
+      StInContent,   ///< inside a text
     } state;
 
   public:
@@ -60,37 +64,53 @@ class xmlWriter_c
     /** open an XML writer, wirte header */
     xmlWriter_c(std::ostream & str);
 
+    /** all tags must be closed by now, otherwise an exception is thrown */
     ~xmlWriter_c(void);
 
+    /** start a new tag */
     void newTag(const std::string & name);
 
-    /**
-     * add an attribute to the current tag
+    /** \name add an attribute to the current tag.
+     *
+     * You may only call this function right after newTag, otherwise an exception is thrown.
      */
+    //@{
     void newAttrib(const std::string & attrib, const std::string & value);
     void newAttrib(const std::string & attrib, unsigned long value);
     void newAttrib(const std::string & attrib, signed long value);
     void newAttrib(const std::string & attrib, unsigned int value) { newAttrib(attrib, (unsigned long)value); }
     void newAttrib(const std::string & attrib, signed int value)  { newAttrib(attrib, (signed long)value); }
+    //@}
 
     /**
-     * ends the current tag.
+     * ends the current tag and pops it off the tag stack
      * when no content has been written to this tag, the short form is used
      * otherwise a normal and tag is written
      */
     void endTag(const std::string & name);
 
-    /** adds text to the content of the current tag */
+    /** \name adds text to the content of the current tag */
+    //@{
     void addContent(const std::string & text);
     void addContent(unsigned long val);
     void addContent(signed long val);
     void addContent(unsigned int val) { addContent((unsigned long)val); }
     void addContent(signed int val) { addContent((signed long)val); }
+    /** get an outstream to output content
+     * You may use this stream to write out content if all the other
+     * functions are too unhandy.
+     * Be careful to not use xml special characters (like <, >, &, and so on) or
+     * confert them properly.
+     * Also don't use this stream once you called any other function from this class
+     * You need to get it again using this function.
+     */
     std::ostream & addContent(void);
+    //@}
 };
 
 // this parser is taken from wsdlpull.sf.net, but heavily modified
 
+/** this class is thriown by the xml parser */
 class xmlParserException_c
 {
   public:
@@ -114,7 +134,12 @@ class xmlParserException_c
     int line, col;
 };
 
-/** A simple, pull based XML parser.  */
+/** A simple, pull based XML parser.
+ *
+ * As this parser is taken from an other project I don't understand it completely
+ * I only comment the functions that I used...
+ *
+ * */
 class xmlParser_c
 {
   public:
@@ -134,10 +159,14 @@ class xmlParser_c
 
     bool isWhitespace(void);
 
+    /** return the text of the current text node.
+     * Only call when you are actually in a text node.
+     */
     std::string getText(void);
 
     const char *getTextCharacters(int *poslen);
 
+    /** get the name of the current tag */
     std::string getName(void)
     {
       return name;
@@ -150,14 +179,21 @@ class xmlParser_c
 
     bool isEmptyElementTag(void);
 
+    /** get the number of attributes in the current tag */
     int getAttributeCount(void)
     {
       return attributeCount;
     }
 
+    /** get the name of one of the attributes */
     std::string getAttributeName(int index);
     std::string getAttributePrefix(int index);
+    /** get the value of one of the attributes */
     std::string getAttributeValue(int index);
+
+    /** get the value of the given attribut.
+     * If the attribut doesn't exist an empty string is returned
+     */
     std::string getAttributeValue(std::string name);
 
     int getEventType(void)
@@ -165,26 +201,37 @@ class xmlParser_c
       return type;
     }
 
-    //parsing methods
+    /** go to the next xml event, and return the event type */
     int next(void);
     int nextToken(void);
     int nextTag(void);
     void prevTag(void);
 
-    //----------------------------------------------------------------------
-    // utility methods to make XML parsing easier ...
+    /** check, if the parser is in a certain state.
+     *
+     * type is one of the even type enum, name is used when a tag name
+     * is required, e.g
+     * \verbatim require(xmlParser_c::START_TAG, "puzzle") \endverbatim
+     * to enforce that we are at an open puzzle tag
+     */
     void require(int type, std::string name);
+
     std::string nextText(void);
+
+    /** skip the complete tree until the close of the current tag.
+     * The stream must be at the beginning of a tag, otherwise an exception
+     * will be thrown
+     */
     void skipSubTree(void);
 
-    //enum for event types
+    /** possible xml events */
     enum
     {
       START_DOCUMENT,
       END_DOCUMENT,
-      START_TAG,
-      END_TAG,
-      TEXT,
+      START_TAG,               ///< an open tag (can be both tag types)
+      END_TAG,                 ///< the end of a node (either closing tag, or short tag)
+      TEXT,                    ///< a text node
       CDSECT,
       ENTITY_REF,
       IGNORABLE_WHITESPACE,
@@ -193,7 +240,10 @@ class xmlParser_c
       DOCDECL,
       UNKNOWN
     };
-    // throws XmlPullParserException with the current line and col
+
+    /** throws XmlPullParserException with the current line and col.
+     * You may use this, if you have encounteted a faulty XML, e.g missing attribute or what not.
+     */
     void exception (std::string desc);
 
   private:
