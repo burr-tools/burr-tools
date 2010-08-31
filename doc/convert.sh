9aa45c0c714583f678b939403edb6748cfd81117
Andreas Röver <roever@users.sourceforge.net>
2010-08-31 19:26:12 +0000

    bigger resolution screenshots for user guide, but still downscaled for online helpt end web page

--- doc/convert.sh
+++ doc/convert.sh
@@ -19,9 +19,11 @@ chmod a-w userGuide.t2t   # just to make sure we don't start editing this file
 
 
 # create a table of content for easier navigation in the t2t file
-../doc_src/txt2tags.py --toc-only userGuide.t2t > userGuide.t2t.toc
+#../doc_src/txt2tags.py --toc-only userGuide.t2t > userGuide.t2t.toc
 
 # create a pfd by first converting t2t to tex file and then running pdflatex
+#----------------------------------------------------------------------------
+
 # run pdflatex 4 times to be sure all references are correct
 ../doc_src/txt2tags.py -t tex --toc -n -o - userGuide.t2t > userGuide.tex
 pdflatex userGuide.tex
@@ -30,6 +32,7 @@ pdflatex userGuide.tex
 pdflatex userGuide.tex
 mv userGuide.pdf burrtools-$title-A4.pdf
 
+# remplace A4 papersize with letter for the americans
 sed 's/documentclass\[11pt,a4paper\]{scrbook}/documentclass[11pt,letterpaper]{scrbook}/' -i userGuide.tex
 pdflatex userGuide.tex
 pdflatex userGuide.tex
@@ -38,7 +41,27 @@ pdflatex userGuide.tex
 mv userGuide.pdf burrtools-$title-letter.pdf
 
 # create the html documentation
-# first create the html output dir, or empty it, then convert the t2t to html
+#------------------------------
+
+#rescale the images to half resolution (only the big window screenshots), DPI resolution doesn't matter here anymore
+# the quality option with mogrify and png files spefizies the compression level
+IMG=""
+IMG="$IMG Window_Anaglyph.png"
+IMG="$IMG Window_AssmImport.png"
+IMG="$IMG Window_Clarissa.png"
+IMG="$IMG Window_Config.png"
+IMG="$IMG Window_DDD_0.png"
+IMG="$IMG Window_DDD_1.png"
+IMG="$IMG Window_DDD_2.png"
+IMG="$IMG Window_DDD_3.png"
+IMG="$IMG Window_ImageExport.png"
+IMG="$IMG Window_Placements.png"
+IMG="$IMG Window_StartUp.png"
+IMG="$IMG Window_Status.png"
+IMG="$IMG Window_StlExport.png"
+for i in $IMG; do mogrify -resize 50% +dither -colors 64 -quality 100 $i; done
+
+# now create the html output dir, or empty it, then convert the t2t to html
 # then split the generated big html file into multiple htmls using htmldoc
 if [ -d html ]
 then
